ff8975d2bca45930f4b7e86700bc14670a6dcedf
sequ3nce <prbar002@gmail.com>
2024-01-24 12:10:05 +0300

    init commit

---
+++ src/main/java/com/barabanov/backup/ReliableSecretBackupApplication.java
@@ -0,0 +1,15 @@
+package com.barabanov.backup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ReliableSecretBackupApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ReliableSecretBackupApplication.class, args);
+	}
+
+}
